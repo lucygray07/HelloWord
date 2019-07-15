3fc960360eec91ecec047e53495516d22f08524e
lucygray07 <52918149+lucygray07@users.noreply.github.com>
2019-07-15 17:05:28 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,12 @@
+class first
+{
+public static void main (String a[])
+{
+System.out.println("This is Hello World");
+main(20);
+}
+public static void main (int k)
+{
+System.out.println("This is Hello World for second main=="+k);
+}
+}
